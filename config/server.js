17a3806947118a2e99baf969678709447010b73c
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2021-08-29 01:32:40 -0400

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2955fb15d097f94edda37928d5833647'),
+    },
+  },
+});
